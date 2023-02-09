1a3a1f8e663d2b5b3fdc422334b552a9b0986596
Dolapo Soares <dsoares@econnectnp.com>
2023-02-09 14:27:50 +0100

    setting up the firebase configuration

---
+++ slack/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC0ZDyqfkGpjRizikuyzg4_9LgnwjaWagU",
+    authDomain: "slack-clone-c02d4.firebaseapp.com",
+    projectId: "slack-clone-c02d4",
+    storageBucket: "slack-clone-c02d4.appspot.com",
+    messagingSenderId: "745997420275",
+    appId: "1:745997420275:web:e87c6d26b52ce6b55a5986",
+    measurementId: "G-BKZ5YE2Q8J"
+  };
