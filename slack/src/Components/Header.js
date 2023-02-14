55fe8ff1210df314822c36c852aecf40cc98ecbc
Dolapo Soares <dsoares@econnectnp.com>
2023-02-14 13:42:47 +0100

    creating the header component

--- slack/src/Components/Header.js
+++ slack/src/Components/Header.js
@@ -1,9 +1,43 @@
  import React from 'react'
+ import styled from "styled-components"
+ import { Avatar } from "@mui/material"
+ import AccessTimeIcon from "@mui/icons-material/AccessTime"
  
  function Header() {
    return (
-     <div>Header</div>
+     <HeaderStyle>
+      {/* Header Left */}
+      <HeaderLeft>
+        <HeadAvatar />
+        <AccessTimeIcon />
+      </HeaderLeft>
+     </HeaderStyle>
    )
  }
  
- export default Header
+ export default Header;
+
+ const HeaderStyle = styled.div`
+    display: flex;
+    position: fixed;
+    width: 100%;
+    padding: 10px 0;
+    justify-content: space-between;
+    background-color: var(--slack-color);
+
+ `
+ const HeaderLeft = styled.div`
+    flex: 0.3;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+
+    > .MuiSvgIcon-root {
+      margin-left: auto;
+      margin-right: 30px;
+    }
+ `
+
+ const HeadAvatar = styled(Avatar)`
+
+ `;
