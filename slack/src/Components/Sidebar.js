dd58b2433fd9ad7a6ab67ac61083c675b14dcbed
Dolapo Soares <dsoares@econnectnp.com>
2023-03-16 13:31:29 +0100

    Styled the header of the sidebar

--- slack/src/Components/Sidebar.js
+++ slack/src/Components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord'
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecordSharp'
+import CreateIcon from '@mui/icons-material/Create'
 
 const Sidebar = () => {
   return (
@@ -13,6 +14,7 @@ const Sidebar = () => {
                     Soares Dolapo
                 </h3>
             </SidebarHead1>
+            <CreateIcon />
         </SidebarHeader>
     </SidebarContainer>
   )
@@ -22,12 +24,41 @@ export default Sidebar
 
 const SidebarContainer = styled.div`
   background-color: var(--slack-color);
+  color: white;
+  flex: 0.3;
+  max-width: 270px;
+  margin-top: 60px;
 `
 
 const SidebarHeader = styled.div`
+  display: flex;
+  border-bottom: 1px solid #49274b;
+  border-top: 1px solid #49274b;
+  padding: 13px;
 
+
+  > .MuiSvgIcon-root {
+    padding: 8px;
+    color: #49274b;
+    font-size: 18px;
+    background-color: white;
+    border-radius: 999px;
+  }
 `
 const SidebarHead1 = styled.div`
+    flex: 1;
 
+    >  h2 {
+      font-size: 15px;
+      font-weight: 900;
+      margin-bottom: 5px;
+    }
+
+    >  h3 {
+      display: flex;
+      font-size: 13px;
+      font-weight: 400;
+      align-items: center;
+    }
 `
 
